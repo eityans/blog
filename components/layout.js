5d2571cc88376cc92927c8d5729ed37a74f36e43
eityans <eityans@gmail.com>
2022-02-12 07:59:15 +0900

    npm install gray-matter

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import utilStyles from '../styles/utils.module.css'
 import styles from './layout.module.css'
 
-const name = 'Your Name'
+const name = 'eityans'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
