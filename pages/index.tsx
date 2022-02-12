c3a56a7955725df4fcdd21efe6b5016d5b393d58
eityans <eityans@gmail.com>
2022-02-12 16:07:05 +0900

    use contentful

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,19 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Date from '../components/date'
 import Layout, { siteTitle } from '../components/layout'
+import { createClient } from '../lib/contentful'
 import { getSortedPostsData } from '../lib/posts'
 import utilStyles from '../styles/utils.module.css'
-
 
 export default function Home({ allPostsData }: {allPostsData: {
   date: string
   title: string
   id: string
 }[]}) {
+  const client = createClient();
+  client.getEntry("1GkLzIGsZ7MHjtKYM8K48I")
+  .then(entry => console.log(entry))
+  .catch(err => console.log(err));
   return (
     <Layout home>
       <Head>
