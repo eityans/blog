258b81b6839b76ce774910fbaadbfc04c5563ba3
eityans <eityans@gmail.com>
2022-02-12 17:21:29 +0900

    get title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,12 @@
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
-import Link from 'next/link'
-import Date from '../components/date'
 import Layout, { siteTitle } from '../components/layout'
 import { createClient } from '../lib/contentful'
-import { getSortedPostsData } from '../lib/posts'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home({ allPostsData }: {allPostsData: {
-  date: string
-  title: string
-  id: string
-}[]}) {
-  const client = createClient();
-  client.getEntry("1GkLzIGsZ7MHjtKYM8K48I")
-  .then(entry => console.log(entry))
-  .catch(err => console.log(err));
+export default function Home({ posts }) {
+  console.log(posts);
+  console.log(posts.items[0].fields.title);
   return (
     <Layout home>
       <Head>
@@ -28,7 +19,17 @@ export default function Home({ allPostsData }: {allPostsData: {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+
+          {posts &&
+            posts.items.map((post) => (
+
+
+              <div>
+                <p>{post.fields.title}</p>
+                {/* <p>{post.fields.content}</p> */}
+              </div>
+            ))}
+          {/* {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>
                 <a>{title}</a>
@@ -38,7 +39,7 @@ export default function Home({ allPostsData }: {allPostsData: {
                 <Date dateString={date} />
               </small>
             </li>
-          ))}
+          ))} */}
         </ul>
       </section>
     </Layout>
@@ -46,10 +47,38 @@ export default function Home({ allPostsData }: {allPostsData: {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
+  const response = await getAllPosts();
+
+  // responseが無ければ404にリダイレクトする
+  if (!response) {
+    return {
+      notFound: true
     }
+  }
+  return {
+    props: { posts: response }
+  }
+  // const allPostsData = getSortedPostsData()
+  // return {
+  //   props: {
+  //     allPostsData
+  //   }
+  // }
+}
+
+const getAllPosts = async () => {
+  const client = createClient()
+  try {
+    const response = await client
+      .getEntries({
+        content_type: "post",
+        order: "-sys.createdAt",
+      })
+      .catch((error) => {
+        return error
+      })
+    return response
+  } catch(error) {
+    throw new Error(error.message)
   }
 }
