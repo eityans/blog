685230063c5214ea833522328c3e2ade48b7a1c1
eityans <eityans@gmail.com>
2022-02-13 16:06:05 +0900

    pass path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
 import { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { GoogleAnalytics } from "../components/GoogleAnalytics";
+import { existsGaId, pageview } from "../lib/gtag";
 import "../styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!existsGaId) {
+      return;
+    }
+
+    const handleRouteChange = (path) => {
+      pageview(path);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
       <GoogleAnalytics />
