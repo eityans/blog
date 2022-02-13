8ad852af1af3a291af117f1c017c929b19289f96
eityans <eityans@gmail.com>
2022-02-14 00:02:59 +0900

    embed tweet

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,7 +1,8 @@
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { BLOCKS } from "@contentful/rich-text-types";
+import { BLOCKS, INLINES } from "@contentful/rich-text-types";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Head from "next/head";
+import { TwitterTweetEmbed } from 'react-twitter-embed';
 import Date from "../../components/date";
 import Layout from "../../components/layout";
 import { getAllPosts, getPostData, Post as PostData } from "../../lib/posts";
@@ -20,17 +21,17 @@ export default function Post({ post }: { post: PostData }) {
         <div className={utilStyles.lightText}>
           <Date dateString={post.createdOn} />
         </div>
-        <blockquote  className="twitter-tweet"><p lang="ja" dir="ltr">スマイルプリキュア</p>&mdash; 橋下徹 (@hashimoto_lo) <a href="https://twitter.com/hashimoto_lo/status/340640143058825216?ref_src=twsrc%5Etfw">June 1, 2013</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+
         {documentToReactComponents(post.content, {
           renderNode: {
             [BLOCKS.EMBEDDED_ASSET]: (node) => <img src={"https:" + node.data.target.fields.file.url} width={600} />,
-            // ["embedded-entry-block"]: (node) => (
-            //   <>
-            //     <TwitterTweetEmbed
-            //       tweetId={'340640143058825216'}
-            //     />
-            //     <b>aaaa</b><br/><br/><br/><p>sss</p>
-            //   </>),
+            [INLINES.HYPERLINK]: (node) => {
+              if (node.data.uri.indexOf("twitter.com") !== -1) {
+                const tweetID = node.data.uri.match(/\d+$/)[0];
+                return <TwitterTweetEmbed tweetId={tweetID} />;
+              }
+              return <></>;
+            },
           },
         })}
       </article>
