b2ede357a145005ac181455d159b442b6dd6a020
eityans <eityans@gmail.com>
2022-02-13 16:16:37 +0900

    hooks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,11 @@
 import { AppProps } from "next/app";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import { GoogleAnalytics } from "../components/GoogleAnalytics";
-import { existsGaId, pageview } from "../lib/gtag";
+import usePageView from "../hooks/usePageView";
 import "../styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
+  usePageView();
 
-  useEffect(() => {
-    if (!existsGaId) {
-      return;
-    }
-
-    const handleRouteChange = (path) => {
-      pageview(path);
-    };
-
-    router.events.on("routeChangeComplete", handleRouteChange);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
   return (
     <>
       <GoogleAnalytics />
