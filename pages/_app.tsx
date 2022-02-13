19a2ff8d4c2545a248a6bbd72a5e4cc016dccf7b
eityans <eityans@gmail.com>
2022-02-13 14:44:03 +0900

    add grag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
+import { existsGaId, GA_ID } from "../lib/gtag";
 import "../styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Google Analytics */}
+        {existsGaId && (
+          <>
+            <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_ID}', {
+                    page_path: window.location.pathname,
+                  });`,
+              }}
+            />
+          </>
+        )}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
