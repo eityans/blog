4419799dcbaa58d3933941d1e6ad823a26fb5e2d
eityans <eityans@gmail.com>
2022-02-13 16:01:52 +0900

    use Script

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -1,24 +1,20 @@
-import Head from "next/head";
+import Script from "next/script";
 import { existsGaId, GA_ID } from "../lib/gtag";
 
 export const GoogleAnalytics = () => (
-  <Head>
-    {/* Google Analytics */}
+  <>
     {existsGaId && (
       <>
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${GA_ID}', {
-                    page_path: window.location.pathname,
-                  });`,
-          }}
-        />
+        <Script defer src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} strategy="afterInteractive" />
+        <Script id="ga" defer strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}');
+          `}
+        </Script>
       </>
     )}
-  </Head>
+  </>
 );
