e45919f00b4149a442ff03b7874001c3bc1afa1a
eityans <eityans@gmail.com>
2022-02-13 21:41:49 +0900

    wip

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -2,12 +2,15 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { BLOCKS } from "@contentful/rich-text-types";
 import { GetStaticPaths, GetStaticProps } from "next";
 import Head from "next/head";
+import { TwitterTweetEmbed } from 'react-twitter-embed';
 import Date from "../../components/date";
 import Layout from "../../components/layout";
 import { getAllPosts, getPostData, Post as PostData } from "../../lib/posts";
 import utilStyles from "../../styles/utils.module.css";
 
 export default function Post({ post }: { post: PostData }) {
+  console.log(post.content);
+  //<>{node.data.target.fields.body}</>,
   return (
     <Layout>
       <Head>
@@ -18,10 +21,17 @@ export default function Post({ post }: { post: PostData }) {
         <div className={utilStyles.lightText}>
           <Date dateString={post.createdOn} />
         </div>
+        <blockquote class="twitter-tweet"><p lang="ja" dir="ltr">スマイルプリキュア</p>&mdash; 橋下徹 (@hashimoto_lo) <a href="https://twitter.com/hashimoto_lo/status/340640143058825216?ref_src=twsrc%5Etfw">June 1, 2013</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         {documentToReactComponents(post.content, {
           renderNode: {
-            // eslint-disable-next-line react/display-name
             [BLOCKS.EMBEDDED_ASSET]: (node) => <img src={"https:" + node.data.target.fields.file.url} width={600} />,
+            ["embedded-entry-block"]: (node) => (
+              <>
+                <TwitterTweetEmbed
+                  tweetId={'340640143058825216'}
+                />
+                <b>aaaa</b><br/><br/><br/><p>sss</p>
+              </>),
           },
         })}
       </article>
