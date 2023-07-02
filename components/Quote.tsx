cde5b50f7f5b586d162eb0820a5cd197282da135
eityans <eityans@gmail.com>
2023-07-02 09:04:20 +0900

    css

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
+import style from "./Quote.module.css";
 
 type Props = {
   children: React.ReactNode;
 };
 
 export const Quote: React.FC<Props> = (props) => {
-  return <>{props.children}</>;
+  return <div className={style.blockquote}>{props.children}</div>;
 };
